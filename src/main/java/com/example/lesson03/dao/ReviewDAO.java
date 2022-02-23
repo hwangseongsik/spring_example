995fafe36a14fca5a2031be2f384e6fd66a66741
seongsik <hss8559@naver.com>
2022-02-23 13:18:45 +0900

    초기 환경 세팅

---
+++ src/main/java/com/example/lesson03/dao/ReviewDAO.java
@@ -0,0 +1,21 @@
+package com.example.lesson03.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.lesson03.model.Review;
+
+@Repository
+public interface ReviewDAO {
+
+	public Review selectReview(int id);
+	
+	public int insertReview(Review review);
+	
+	public int insertReviewAsField(
+			@Param("storeId") int storeId,
+			@Param("menu") String menu,
+			@Param("userName") String userName,
+			@Param("point") double point,
+			@Param("review") String review);
+}
